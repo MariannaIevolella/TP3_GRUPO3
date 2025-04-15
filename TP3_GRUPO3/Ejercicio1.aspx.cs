fc814a03657bc8a1b844840676f42bdaa0ab6906
Marianna <mariannai1999@gmail.com>
2025-04-15 19:13:50 -0300

    Agregué validaciones al guardar una localidad; verifico que el campo no esté vacío antes de guardar; permito ingresar números en el nombre de la localidad, como por ejemplo “25 de mayo”; controlo que no se repitan localidades ya ingresadas; al guardar, añado la localidad al DropDownList en la sección de usuarios; luego de guardar, limpio el TextBox automáticamente; si el usuario intenta guardar sin escribir nada, muestro el mensaje “Ingrese localidad

--- TP3_GRUPO3/Ejercicio1.aspx.cs
+++ TP3_GRUPO3/Ejercicio1.aspx.cs
@@ -11,7 +11,45 @@ namespace TP3_GRUPO3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Page.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+        protected void btnGuardarLocalidad_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid) /// verifica que los validadores pasaron correctamente
+            {
+                string localidad = txtLocalidad.Text.Trim();  /// se guarda lo que escribio el usuario; 
+
+                    if (localidad.All(char.IsDigit))
+                    {
+                    lblMensaje.ForeColor = System.Drawing.Color.Red;
+                    lblMensaje.Text = "El nombre de localidad no puede ser solo números.";
+                    return;
+                    }
+
+                ddlLocalidades.Items.Add(localidad); ///se agrega al DropDownList
+                txtLocalidad.Text = ""; /// se limpia el TextBox
+                lblMensaje.ForeColor = System.Drawing.Color.Green;
+                lblMensaje.Text = "Localidad guardada correctamente.";
+            }
+            
 
         }
+        protected void txtLocalidad_TextChanged(object sender, EventArgs e)
+        {
+            lblMensaje.Text = "";
+        }
+        protected void cvRepetida_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            string nuevaLocalidad = args.Value.Trim().ToLower();/// texto que el usuario ingreso; elimina espacios en blanco;convierte todo a minuscula.
+
+            bool existe = ddlLocalidades.Items /// accede a todo el DropDownList
+                            .Cast<ListItem>()
+                            .Any(item => item.Text.Trim().ToLower() == nuevaLocalidad); ///busca si alguno de esos ítems es igual al que se está intentando ingresar.
+                                     /// item.Text.Trim().ToLower(): Recorro cada ítem del DropDownList y me fijo si alguno tiene el mismo texto, en mayúsculas y sin espacios, que el que el usuario ingreso.
+            args.IsValid = !existe; /// para decir si es valido 
+        }
+
+        
     }
 }
